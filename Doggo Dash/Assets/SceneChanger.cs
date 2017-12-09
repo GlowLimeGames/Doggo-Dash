318c2bd681c741d7b26b5eb00117410653907c71
bryan_prieto <bap14@hampshire.edu>
2017-12-09 16:48:49 -0500

    start screen

---
+++ Doggo Dash/Assets/SceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToChangeTo);
+    }
+
+}
+
